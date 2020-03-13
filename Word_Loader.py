56db150325d388a8197f626b1cc917e98f74b679
GanzeVA <szymonosis@o2.pl>
2020-03-13 21:33:19 +0100

    Added basic function to classify words into 3 difficulties (easy, normal, hard) by length

--- Word_Loader.py
+++ Word_Loader.py
@@ -5,14 +5,35 @@ import random as rand
 base_directory = "\\Words"
 base_language = "Polish"
 
+normal_diff = 6
+hard_diff = 10
 
+
+# difficulty: -1 random; 0 easy; 1 normal; 2 hard
 def load_word(language=base_language, difficulty=-1):
     chosen_category_path = __choose_category(language)
     with open(chosen_category_path) as p:
         words = p.readlines()
         if difficulty == -1:
             return words[rand.randint(0, len(words) - 1)].strip()
+        return __word_by_difficulty(words, difficulty)
+
+
+def __word_by_difficulty(words, difficulty):
+    if difficulty not in [0, 1, 2]:
         return "unsupported"
+
+    if difficulty == 2:
+        nwords = [w for w in words if len(w) >= hard_diff]
+        if len(nwords) == 0:
+            difficulty = 1
+    if difficulty == 1:
+        nwords = [w for w in words if normal_diff <= len(w) < hard_diff]
+        if len(nwords) == 0:
+            difficulty = 0
+    if difficulty == 0:
+        nwords = [w for w in words if len(w) < normal_diff]
+    return nwords[rand.randint(0, len(nwords) - 1)].strip()
 
 
 def __choose_category(language):
@@ -21,4 +42,4 @@ def __choose_category(language):
     return join(path, category_files[rand.randint(0, len(category_files) - 1)])
 
 
-print(load_word())
+print(load_word("Polish", 2))
