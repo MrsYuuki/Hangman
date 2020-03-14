cc16b7352a8413c2cd4a5f14fb5772516b41b2b1
GanzeVA <szymonosis@o2.pl>
2020-03-14 16:47:29 +0100

    GameMaster passes score value

--- GameMaster.py
+++ GameMaster.py
@@ -9,13 +9,13 @@ cur_tries = 0
 max_tries = 5
 
 
-def initialize_game(difficulty, language="Polish"):
-    global cur_tries, current_word, used_letters, used_words
+def initialize_game(difficulty=-1, language="Polish"):
+    global cur_tries, current_word, used_letters, used_words, score
     current_word = wordloader.load_word(language, difficulty, used_words).upper()
     used_words.append(current_word)
     used_letters = []
     cur_tries = 0
-    return [word_hider(), used_letters, 0]
+    return [word_hider(), used_letters, 0, score]
 
 
 def update_game(letter):
@@ -26,10 +26,10 @@ def update_game(letter):
     cur_tries += 1
     if "_" not in hided_word: #win
         score += 1
-        return [hided_word, used_letters, 1]
+        return [hided_word, used_letters, 1, score]
     elif cur_tries == max_tries: #lose
-        return [hided_word, used_letters, 2]
-    return [hided_word, used_letters, 0]
+        return [hided_word, used_letters, 2, score]
+    return [hided_word, used_letters, 0, score]
 
 
 def word_hider():
