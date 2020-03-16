4100e22a1c884d1964ea01aad24721db772910c7
GanzeVA <szymonosis@o2.pl>
2020-03-16 03:11:53 +0100

    Fixed display of words with 2 or more bytes utf-8 characters

--- GameMaster.py
+++ GameMaster.py
@@ -12,7 +12,7 @@ max_tries = 5
 
 def initialize_game(difficulty=-1, language="Polish"):
     global cur_tries, current_word, used_letters, used_words, score
-    current_word = wordloader.load_word(language, difficulty, used_words).upper()
+    current_word = additional_upper(wordloader.load_word(language, difficulty, used_words).upper())
     used_words.append(current_word)
     used_letters = []
     cur_tries = 0
@@ -21,8 +21,8 @@ def initialize_game(difficulty=-1, language="Polish"):
 
 def update_game(letter):
     global cur_tries, used_letters, score
-    letter = letter.upper()
-    used_letters.append(letter)
+    letter = additional_upper(letter.upper())
+    used_letters.append(letter.decode("utf-8"))
     hided_word = word_hider()
     cur_tries += 1
     if "_" not in hided_word: #win
@@ -36,9 +36,22 @@ def update_game(letter):
 def word_hider():
     global used_letters, current_word
     hided_word = ""
-    for l in current_word:
+    for l in current_word.decode("utf-8"):
         if l in used_letters or l.isspace():
             hided_word += l
         else:
             hided_word += "_"
     return hided_word
+
+
+def additional_upper(word):
+    word = word.replace("ą", "Ą")
+    word = word.replace("ć", "Ć")
+    word = word.replace("ę", "Ę")
+    word = word.replace("ł", "Ł")
+    word = word.replace("ń", "Ń")
+    word = word.replace("ó", "Ó")
+    word = word.replace("ś", "Ś")
+    word = word.replace("ż", "Ż")
+    word = word.replace("ź", "Ź")
+    return word
