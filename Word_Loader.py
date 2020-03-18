7fce5996a2f2319d294066898e9d5ad1c87ad6ad
GanzeVA <szymonosis@o2.pl>
2020-03-18 15:48:01 +0100

    Words correctly load from all categories according to the set difficulty

--- Word_Loader.py
+++ Word_Loader.py
@@ -12,14 +12,13 @@ hard_diff = 10
 
 # difficulty: -1 random; 0 easy; 1 normal; 2 hard
 def load_word(language=base_language, difficulty=-1, used_words=[]):
-    chosen_category_path = __choose_category(language)
-    with open(chosen_category_path, encoding='utf-8') as p:
-        words = [w for w in p.readlines() if w not in used_words]
-        if len(words) == 0:
-            return load_word(language, difficulty, used_words)
-        if difficulty == -1:
-            return words[rand.randint(0, len(words) - 1)]
-        return __word_by_difficulty(words, difficulty)
+    all_words = __load_words_lang(language, used_words)
+    words = [w for w in all_words if w[0] not in used_words]
+    if len(words) == 0:
+        words = all_words
+    if difficulty == -1:
+        return words[rand.randint(0, len(words) - 1)]
+    return __word_by_difficulty(words, difficulty)
 
 
 def __word_by_difficulty(words, difficulty):
@@ -27,19 +26,24 @@ def __word_by_difficulty(words, difficulty):
         return "unsupported"
 
     if difficulty == 2:
-        nwords = [w for w in words if len(w) >= hard_diff]
+        nwords = [w for w in words if len(w[0]) >= hard_diff]
         if len(nwords) == 0:
             difficulty = 1
     if difficulty == 1:
-        nwords = [w for w in words if normal_diff <= len(w) < hard_diff]
+        nwords = [w for w in words if normal_diff <= len(w[0]) < hard_diff]
         if len(nwords) == 0:
             difficulty = 0
     if difficulty == 0:
-        nwords = [w for w in words if len(w) < normal_diff]
+        nwords = [w for w in words if len(w[0]) < normal_diff]
     return nwords[rand.randint(0, len(nwords) - 1)]
 
 
-def __choose_category(language):
+def __load_words_lang(language, used_words):
     path = getcwd() + base_directory + "\\" + language
     category_files = [f for f in listdir(path) if isfile(join(path, f))]
-    return join(path, category_files[rand.randint(0, len(category_files) - 1)])
+    all_words = []
+    for cat in category_files:
+        with open(join(path, cat), encoding='utf-8') as p:
+            for w in p.readlines():
+                all_words.append((w, cat))
+    return all_words
